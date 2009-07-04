7522ee5158c0ebac735ad9390a34137e0d407a6d
jaderd <jaderd@eea57fb2-63f0-11de-81b3-7dd168245822>
2009-07-04 02:11:06 +0000

    Refactored and cleared code

--- WaveReader/WaveReader/WaveReader.cs
+++ WaveReader/WaveReader/WaveReader.cs
@@ -7,12 +7,14 @@ namespace WaveReaderDLL
 {
     public class WaveReader
     {
+        private static readonly int BitsPerByte = 8;
+        private static readonly int MaxBits = 8;
         public Int32[][] Data { get; private set; }
         public int CompressionCode { get; private set; }
         public int NumberOfChannels { get; private set; }
         public int SampleRate { get; private set; }
-        public int AverageBytesPerSecond { get; private set; }
-        public int SignificantBitsPerSample { get; private set; }
+        public int BytesPerSecond { get; private set; }
+        public int BitsPerSample { get; private set; }
         public int BlockAlign { get; private set; }
         public int Frames { get; private set; }
         public double TimeLength { get; private set; }
@@ -23,123 +25,62 @@ namespace WaveReaderDLL
         /// <param name="stream">Input WAVE file stream</param>
         public WaveReader(Stream stream)
         {
-            BinaryReader br = new BinaryReader(stream);
-            try
+            using (BinaryReader binaryReader = new BinaryReader(stream))
             {
-                if (new string(br.ReadChars(4)).ToUpper() == "RIFF")
+                binaryReader.ReadChars(4); //"RIFF"
+                int length = binaryReader.ReadInt32();
+                binaryReader.ReadChars(4); //"WAVE"
+                string chunkName = new string(binaryReader.ReadChars(4)); //"fmt "
+                int chunkLength = binaryReader.ReadInt32();
+                this.CompressionCode = binaryReader.ReadInt16(); //1 for PCM/uncompressed
+                this.NumberOfChannels = binaryReader.ReadInt16();
+                this.SampleRate = binaryReader.ReadInt32();
+                this.BytesPerSecond = binaryReader.ReadInt32();
+                this.BlockAlign = binaryReader.ReadInt16();
+                this.BitsPerSample = binaryReader.ReadInt16();
+                if ((MaxBits % BitsPerSample) != 0)
                 {
-                    int length = br.ReadInt32();
-                    if (new string(br.ReadChars(4)).ToUpper() == "WAVE")
+                    throw new Exception("The input stream uses an unhandled SignificantBitsPerSample parameter");
+                }
+                binaryReader.ReadChars(chunkLength - 16);
+                chunkName = new string(binaryReader.ReadChars(4));
+                try
+                {
+                    while (chunkName.ToLower() != "data")
                     {
-                        string chunkName = new string(br.ReadChars(4)); //"fmt "
-                        int chunkLength = br.ReadInt32();
-                        this.CompressionCode = br.ReadInt16(); //1 for PCM/uncompressed
-                        this.NumberOfChannels = br.ReadInt16();
-                        this.SampleRate = br.ReadInt32();
-                        this.AverageBytesPerSecond = br.ReadInt32();
-                        this.BlockAlign = br.ReadInt16();
-                        this.SignificantBitsPerSample = br.ReadInt16();
-                        if (chunkLength > 16)
-                        {
-                            br.ReadChars(chunkLength - 16);
-                        }
-                        chunkName = new string(br.ReadChars(4));
-                        try
-                        {
-                            while (chunkName.ToLower() != "data")
-                            {
-                                br.ReadChars(br.ReadInt32());
-                                chunkName = new string(br.ReadChars(4));
-                            }
-                        }
-                        catch
-                        {
-                            throw new Exception("Input stream misses the data chunk");
-                        }
-                        chunkLength = br.ReadInt32();
-                        this.Frames = 8 * chunkLength / this.SignificantBitsPerSample / this.NumberOfChannels;
-                        this.TimeLength = ((double)this.Frames) / ((double)this.SampleRate);
-                        this.Data = new Int32[this.NumberOfChannels][];
-                        for (int j = 0; j < this.NumberOfChannels; j++)
-                        {
-                            this.Data[j] = new Int32[this.Frames];
-                        }
-                        switch (SignificantBitsPerSample)
-                        {
-                            case 4:
-                                Byte b = new Byte();
-                                bool IsEven = true;
-                                for (int i = 0; i < this.Frames; i++)
-                                {
-                                    for (int j = 0; j < this.NumberOfChannels; j++)
-                                    {
-                                        if (IsEven)
-                                        {
-                                            b = br.ReadByte();
-                                            Data[j][i] = Convert.ToInt32((b >> 4) & 0x0F);
-                                        }
-                                        else
-                                        {
-                                            Data[j][i] = Convert.ToInt32(b & 0x0F);
-                                        }
-                                        IsEven = !IsEven;
-                                    }
-                                }
-                                break;
-                            case 8:
-                                for (int i = 0; i < this.Frames; i++)
-                                {
-                                    for (int j = 0; j < this.NumberOfChannels; j++)
-                                    {
-                                        Data[j][i] = Convert.ToInt32(br.ReadByte());
-                                    }
-                                }
-                                break;
-                            case 16:
-                                for (int i = 0; i < this.Frames; i++)
-                                {
-                                    for (int j = 0; j < this.NumberOfChannels; j++)
-                                    {
-                                        Data[j][i] = br.ReadInt16();
-                                    }
-                                }
-                                break;
-                            case 24:
-                                for (int i = 0; i < this.Frames; i++)
-                                {
-                                    for (int j = 0; j < this.NumberOfChannels; j++)
-                                    {
-                                        Byte[] int24 = br.ReadBytes(3);
-                                        Data[j][i] = Convert.ToInt32(int24[0]) + (Convert.ToInt32(int24[1])<<8) + (Convert.ToInt32(int24[2])<<16);
-                                    }
-                                }
-                                break;
-                            case 32:
-                                for (int i = 0; i < Frames; i++)
-                                {
-                                    for (int j = 0; j < this.NumberOfChannels; j++)
-                                    {
-                                        Data[j][i] = br.ReadInt32();
-                                    }
-                                }
-                                break;
-                            default:
-                                throw new Exception("The input stream uses an unhandled SignificantBitsPerSample parameter");
-                        }
-                    }
-                    else
-                    {
-                        throw new Exception("Input stream doesn't comply with the WAVE specification");
+                        binaryReader.ReadChars(binaryReader.ReadInt32());
+                        chunkName = new string(binaryReader.ReadChars(4));
                     }
                 }
-                else
+                catch
                 {
-                    throw new Exception("Input stream doesn't comply with the RIFF specification");
+                    throw new Exception("Input stream misses the data chunk");
                 }
-            }
-            finally
-            {
-                br.Close();
+                chunkLength = binaryReader.ReadInt32();
+                this.Frames = 8 * chunkLength / this.BitsPerSample / this.NumberOfChannels;
+                this.TimeLength = ((double)this.Frames) / ((double)this.SampleRate);
+                this.Data = new Int32[this.NumberOfChannels][];
+                for (int channel = 0; channel < this.NumberOfChannels; channel++)
+                {
+                    this.Data[channel] = new Int32[this.Frames];
+                }
+                int readedBits = 0;
+                int numberOfReadedBits = 0;
+                for (int frame = 0; frame < this.Frames; frame++)
+                {
+                    for (int channel = 0; channel < this.NumberOfChannels; channel++)
+                    {
+                        while (numberOfReadedBits < this.BitsPerSample)
+                        {
+                            readedBits |= Convert.ToInt32(binaryReader.ReadByte()) << numberOfReadedBits;
+                            numberOfReadedBits += BitsPerByte;
+                        }
+                        int numberOfExcessBits = numberOfReadedBits - BitsPerSample;
+                        this.Data[channel][frame] = readedBits >> numberOfExcessBits;
+                        readedBits = readedBits % (1 << numberOfExcessBits);
+                        numberOfReadedBits = numberOfExcessBits;
+                    }
+                }
             }
         }
     }
